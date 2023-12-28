da6890df6a0cceb0694aafd2acc24c3bf3a5f72b
Thewarden <114976956+Thewarden@users.noreply.github.com>
2023-12-28 11:14:21 +0530

    Add files via upload
    Added maze code

---
+++ main.js
@@ -0,0 +1,32 @@
+var canvas = document.getElementById("renderCanvas");
+
+var engine = new BABYLON.Engine(canvas);
+
+var createScene = function(){
+    var scene = new BABYLON.Scene(engine);
+
+    scene.createDefaultCameraOrLight(true, false, true);
+
+    // var box = new BABYLON.MeshBuilder.CreateGround('',{
+    //     height: 10
+    // })
+
+    var ground = new BABYLON.MeshBuilder.CreateGroundFromHeightMap('','./maze16x16.png',{
+        height: 16,
+        width: 16,
+        depth: 10,
+        subdivisions: 300
+    });
+
+    return scene;
+}
+
+var scene = createScene();
+
+engine.runRenderLoop(function(){
+    scene.render();
+});
+
+window.addEventListener('resize', function(){
+    engine.resize()
+});
