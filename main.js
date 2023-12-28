b5fdc23b26d5f6acf30ea394e05b311aba0c72af
eroxion <anusheelsoni4@gmail.com>
2023-12-28 15:51:45 +0530

    Maze Map Update

--- main.js
+++ main.js
@@ -2,31 +2,31 @@ var canvas = document.getElementById("renderCanvas");
 
 var engine = new BABYLON.Engine(canvas);
 
-var createScene = function(){
-    var scene = new BABYLON.Scene(engine);
+var createScene = function () {
+  var scene = new BABYLON.Scene(engine);
 
-    scene.createDefaultCameraOrLight(true, false, true);
+  scene.createDefaultCameraOrLight(true, false, true);
 
-    // var box = new BABYLON.MeshBuilder.CreateGround('',{
-    //     height: 10
-    // })
+  var ground = new BABYLON.MeshBuilder.CreateGroundFromHeightMap(
+    "",
+    "./maze16x16.png",
+    {
+      height: 16,
+      width: 16,
+      depth: 10,
+      subdivisions: 300,
+    }
+  );
 
-    var ground = new BABYLON.MeshBuilder.CreateGroundFromHeightMap('','./maze16x16.png',{
-        height: 16,
-        width: 16,
-        depth: 10,
-        subdivisions: 300
-    });
-
-    return scene;
-}
+  return scene;
+};
 
 var scene = createScene();
 
-engine.runRenderLoop(function(){
-    scene.render();
+engine.runRenderLoop(function () {
+  scene.render();
 });
 
-window.addEventListener('resize', function(){
-    engine.resize()
+window.addEventListener("resize", function () {
+  engine.resize();
 });
